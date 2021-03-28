4809bcb0705faed21ccb56d7a8cd348930d5deeb
BaraaAlastal <70208732+BaraaAlastal@users.noreply.github.com>
2021-03-29 00:09:04 +0300

    upload service image on firebase by company

--- app/src/main/java/com/moomen/graduationproject/ui/fragment/categories/HallsFragment.java
+++ app/src/main/java/com/moomen/graduationproject/ui/fragment/categories/HallsFragment.java
@@ -1,6 +1,13 @@
 package com.moomen.graduationproject.ui.fragment.categories;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +20,14 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -23,17 +35,25 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.moomen.graduationproject.R;
 import com.moomen.graduationproject.databinding.FragmentHallsBinding;
 import com.moomen.graduationproject.model.Notification;
 import com.moomen.graduationproject.model.Service;
 import com.moomen.graduationproject.model.User;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Random;
+
+import id.zelory.compressor.Compressor;
 
 public class HallsFragment extends Fragment {
 
@@ -43,7 +63,6 @@ public class HallsFragment extends Fragment {
     private FirebaseFirestore firebaseFirestore;
     private StorageReference storageReference;
     private boolean isEmpty = false;
-    private String hallImage = "https://firebasestorage.googleapis.com/v0/b/weddingapp-be318.appspot.com/o/Ads_Image%2F2.jpg?alt=media&token=610fb28e-8c44-4480-8da4-927678d8f401";
     private String hallName;
     private String ownerName;
     private String phone;
@@ -56,6 +75,11 @@ public class HallsFragment extends Fragment {
     private String date;
     private String serviceId;
     private String userId;
+    private static final int MAX_LENGTH = 100;
+    private Uri imageUri = null;
+    private String imageName;
+    private Bitmap compressor;
+    private String downloadUri;
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
@@ -69,16 +93,106 @@ public class HallsFragment extends Fragment {
         return binding.getRoot();
     }
 
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         spinnerCreate(binding.spinnerCity);
+        binding.imageViewHall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cropImage();
+            }
+        });
         binding.buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createHallService();
             }
         });
+    }
+
+    private void cropImage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+            } else {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        //.setMinCropResultSize(512,512)
+                        .setAspectRatio(4, 4)
+                        .start(getContext(), HallsFragment.this);
+            }
+        }
+    }
+
+    private void postHallImageOnFireBase() {
+        if (imageUri != null) {
+            compressAndNameImage();
+            ByteArrayOutputStream byteArrayInputStream = new ByteArrayOutputStream();
+            compressor.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayInputStream);
+            byte[] thumpData = byteArrayInputStream.toByteArray();
+            StorageReference filePath = storageReference.child("Hall_Image/").child(imageName);
+            UploadTask uploadTask = filePath.putBytes(thumpData);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+
+                            return filePath.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                downloadUri = task.getResult().toString();
+                                postHallOnFirebase();
+                            }
+                        }
+
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "The Hall image is required!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void compressAndNameImage() {
+        imageName = random() + ".jpg";
+        File imageFile = new File(imageUri.getPath());
+        try {
+            compressor = new Compressor(getContext())
+                    .setMaxHeight(240)
+                    .setMaxWidth(360)
+                    .setQuality(5)
+                    .compressToBitmap(imageFile);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
     }
 
     private void spinnerCreate(Spinner spinner) {
@@ -113,12 +227,12 @@ public class HallsFragment extends Fragment {
         checkEditText(details, binding.editTextDetail, "Detail");
 
         if (!isEmpty) {
-            Service service = new Service(hallImage, city, hallName, ownerName, phone, location, details, false, new ArrayList<>(), "Halls", date);
-            postHallOnFirebase(service);
+            postHallImageOnFireBase();
         }
     }
 
-    private void postHallOnFirebase(Service service) {
+    private void postHallOnFirebase() {
+        Service service = new Service(downloadUri, city, hallName, ownerName, phone, location, details, false, new ArrayList<>(), "Halls", date);
         firebaseFirestore.collection("Services").add(service).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
             @Override
             public void onComplete(@NonNull Task<DocumentReference> task) {
@@ -163,5 +277,19 @@ public class HallsFragment extends Fragment {
 
     private void makeToast(Context context, String string) {
         Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == getActivity().RESULT_OK) {
+                imageUri = result.getUri();
+                binding.imageViewHall.setImageURI(imageUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
